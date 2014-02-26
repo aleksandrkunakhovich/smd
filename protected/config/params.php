69e06622a60fee7b441ec55682a4fe2f7a623077
aknhvt <aleksandrkunakhovich@gmail.com>
2014-02-26 11:49:38 +0200

    add config

---
+++ protected/config/params.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    # Vanilla API params
+    'vanilla_api_domain' => 'ecommercefuel',
+    'vanilla_api_method' => 'json',
+    'vanilla_api_token' => '838b778d652375eec87cb1484e952289',
+
+    # Other params
+    'users_per_page' => 50,
+];
